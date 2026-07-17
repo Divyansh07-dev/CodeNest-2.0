c22f0c69a4616d15e7b88272109c3c9739174887
Divyansh <divyanshsharma1972005@gmail.com>
2026-07-17 18:44:26 +0530

    Remove old Backend and Frontend folders

--- Backend/src/config/db.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-async function main(){
-    // await mongoose.connect('mongodb://localhost:27017/MyDB', {
-    await mongoose.connect(process.env.DB_CONNECT_STRING )
-}
-module.exports = main;
-
-
